7a9db933da962b1f7df5576fa0cf7a36f994093c
bkayal <bkayal@paypal.com>
2020-11-18 03:23:13 +0530

    Setup Project Structure for Backend

---
+++ backend/sff/src/main/java/com/ohack/sff/SffApplication.java
@@ -0,0 +1,13 @@
+package com.ohack.sff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SffApplication.class, args);
+	}
+
+}
---
+++ backend/sff/src/main/java/com/ohack/sff/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.ohack.sff.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String restricted() {
+        return "Restricted Access";
+    }
+}
